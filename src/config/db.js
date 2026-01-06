0b095809932e2f33fc78439751140d91dcef4f72
Xuan Thang <thangthitotnghiep@gmail.com>
2026-01-07 00:32:10 +0700

    deploy test

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,22 @@
-import { Pool } from "pg"
-import dotenv from 'dotenv'
+import { Pool } from "pg";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const db_host = process.env.DB_HOST
-const db_name = process.env.DB_NAME
-const db_user = process.env.DB_USER
-const db_pass = process.env.DB_PASS
-const port = process.env.DB_PORT || 5432
-const db = new Pool({
-    host: db_host,
-    database: db_name,
-    user: db_user,
-    password: db_pass,
-    port : port
-})
+// Nếu production, dùng DATABASE_URL từ Render, còn dev thì dùng từng biến riêng
+const db = new Pool(
+  process.env.NODE_ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }, // cần cho kết nối cloud
+      }
+    : {
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        port: process.env.DB_PORT || 5432,
+      }
+);
 
-export default db
+export default db;
